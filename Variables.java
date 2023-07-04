786c11157f209ff15baf50958fbcc12ecf73e638
FCPongs <metherelax@gmail.com>
2023-07-04 15:33:40 +0800

    New notes

--- Variables.java
+++ Variables.java
@@ -2,5 +2,8 @@ public class Variables{
     public static void main(String[] args) {
         System.out.print("Variables");
         
+        System.out.println("To print a string like this, use \"\""); //! Use ""
+        System.out.println("To print number, dont use \"\" sample: " + 1); //! Dont use ""
+        System.out.println(1 + 1);
     }
 }
