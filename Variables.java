ec8e447f15c76f886473ead63b440c75b48836a4
FCPongs <metherelax@gmail.com>
2023-07-04 15:37:17 +0800

    Add comments

--- Variables.java
+++ Variables.java
@@ -2,8 +2,16 @@ public class Variables{
     public static void main(String[] args) {
         System.out.print("Variables");
         
+        // This is how you make a comment
+        
+        /*
+        Example of a
+        Multi-line comment
+         */
+
         System.out.println("To print a string like this, use \"\""); //! Use ""
         System.out.println("To print number, dont use \"\" sample: " + 1); //! Dont use ""
-        System.out.println(1 + 1);
+        System.out.println(1 + 1); //! Add
+        System.out.println(2 * 3); //! Multiply
     }
 }
