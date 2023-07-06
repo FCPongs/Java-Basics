d7a24d34e66747f30fe4b13580ee8e4dc8016325
FCPongs <metherelax@gmail.com>
2023-07-06 11:48:11 +0800

    Commit

---
+++ MathJav.java
@@ -0,0 +1,17 @@
+public class MathJav {
+    public static void main(String[] args) {
+        System.out.println("Java Math class has many methods that allows you to perform mathematical tasks on numbers");
+
+        int num1;
+        int num2;
+        //! Max 
+        num1 = Math.max(5,10);
+        System.out.println(num1);
+
+        //! Min
+        num2 = Math.min(10, 25);
+        System.out.println(num2);
+
+        //! Square root
+    }
+}
