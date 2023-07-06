0495867916c0414190a6a2d50824bf72171ac1ff
FCPongs <metherelax@gmail.com>
2023-07-06 12:24:37 +0800

    If else

---
+++ IfElse.java
@@ -0,0 +1,19 @@
+public class IfElse {
+    public static void main(String[] args) {
+
+        boolean checker = true;
+
+        if (checker = true){
+            System.out.println("The condition is True"); //! This will run if checker is true
+        }
+        else
+            System.out.println("The condition is False");
+
+        //! Short Hand if else
+        //* variable = (condition) ? expressionTrue :  expressionFalse; */
+        int time = 20;
+        String check = (time > 18) ? "Legal" : "Minor";
+
+        System.out.println(check);
+    }
+}
