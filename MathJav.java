fbac9cba7d040373356becff72a76a6b45a899e3
FCPongs <metherelax@gmail.com>
2023-07-06 11:57:35 +0800

    New notes

--- MathJav.java
+++ MathJav.java
@@ -2,8 +2,8 @@ public class MathJav {
     public static void main(String[] args) {
         System.out.println("Java Math class has many methods that allows you to perform mathematical tasks on numbers");
 
-        int num1;
-        int num2;
+        double num1,num2,num3,num4,num5;
+    
         //! Max 
         num1 = Math.max(5,10);
         System.out.println(num1);
@@ -13,5 +13,14 @@ public class MathJav {
         System.out.println(num2);
 
         //! Square root
+        num3 = Math.sqrt(64);
+        System.out.println(num3);
+
+        //! Absolute Value
+        num4 = Math.abs(-4.7);
+        System.out.println(num4);
+
+        //! Random number
+        num5 = Math.random();
     }
 }
