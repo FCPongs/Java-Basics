233e674b99627d3577a57e7ba191f922a985f982
FCPongs <metherelax@gmail.com>
2023-07-06 08:08:44 +0800

    New notes

--- Operators.java
+++ Operators.java
@@ -5,7 +5,11 @@ public class Operators {
         System.out.println(x);
         //! Addition assignment operator (+=)
         int y = 10;
-        y += 5;
+        y += 5; //*  Same as: y = y + 5
         System.out.println(y);
+
+        // https://www.w3schools.com/java/java_operators.asp
+
+        
     }
 }
