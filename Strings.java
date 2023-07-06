bb3ab35e6e37e4df6df76b72c97bc190c4f1d718
FCPongs <metherelax@gmail.com>
2023-07-06 08:36:25 +0800

    Notes

--- Strings.java
+++ Strings.java
@@ -1,5 +1,32 @@
 public class Strings {
     public static void main(String[] args) {
-        System.out.println("hello");
+
+        String greeting = "Hello";
+        System.out.println(greeting);
+
+        String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //! Length is strName.length()
+        System.out.println("The length of the txt string is: " + txt.length());
+        System.out.println(txt.toUpperCase());   // Outputs "HELLO WORLD" //! UpperCase is strName.length()
+        System.out.println(txt.toLowerCase());   // Outputs "hello world" //! LowerCase is strName.length()
+
+        //! Finding a Character
+        String txtStr = "Please locate where 'locate' occurs!";
+        System.out.println(txtStr.indexOf("locate"));
+        System.out.println("---------------------------------------------------------------------\n");
+
+        //! CONCATENATION
+        System.out.println("CONCATENATION");
+        String firstName = "John";
+        String lastName = "Doe";
+        String middleName= "Pongase";
+        System.out.println(firstName + " " + lastName);
+        //* OR */
+        System.out.println(firstName.concat(lastName).concat(middleName));
+        System.out.println("---------------------------------------------------------------------\n");
+
+        //! SPECIAL CHARACTERS
+        System.out.println("This is a: \' - Single quote");
+        System.out.println("This is a: \" - Double quote");
+        System.out.println("This is a: \\ - Backslash");
     }
 }
