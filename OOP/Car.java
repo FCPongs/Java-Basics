16509669057b378fa922dc0ac4e87cf4961e66a8
FCPongs <metherelax@gmail.com>
2023-07-08 22:52:23 +0800

    New notes

---
+++ OOP/Car.java
@@ -0,0 +1,16 @@
+package OOP;
+
+public class Car {
+    //! An object can have attributes and methods
+    String color = "White and black";
+    String brand = "Mclaren";
+    String model = "720s";
+    int price = 20000000;
+
+    void turnOn(){
+        System.out.println("The car is turned on.");
+    }
+    void rev(){
+        System.out.println("Vroom vroooom!");
+    }
+}
---
+++ OOP/MainOne.java
@@ -0,0 +1,14 @@
+package OOP;
+
+public class MainOne {
+    public static void main(String[] args) {
+
+        Car myCar = new Car();
+
+        //! Name of the object and the attribute
+        System.out.println(myCar.brand);
+
+        //! Name of the object and the function
+        myCar.rev();
+    }
+}
